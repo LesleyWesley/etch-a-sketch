e352b08eba77321abec7534873898b1b17f9b9f9
lesley_w <lrwesley92@gmail.com>
2023-01-05 21:50:13 -0600

    Wrote javascript to generate grid

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 
 const gridContainer = document.querySelector(".grid-container");
 
+//Selects button to change the grid size
+
+const changeGridButton = document.querySelector(".change-grid");
+console.log(changeGridButton);
+
 //Variable for number of divs across div container
 
 let squareNum = 16;
@@ -10,20 +15,32 @@ let squareNum = 16;
 
 //Builds grid based on whatever squareNum is
 
-const buildGrid = function () {
-  let totalSquares = 0;
+const buildGrid = function (squareAmount) {
+  let totalSquares = 1;
 
-  const gridWidth = gridContainer.offsetWidth - 6;
+  const gridWidth = gridContainer.offsetWidth - 6 - (squareAmount + 3);
+  console.log(gridWidth / squareNum);
 
-  while (totalSquares <= squareNum * squareNum) {
+  while (totalSquares <= squareAmount * squareAmount) {
     const div = document.createElement("div");
     div.classList.add("square");
-    div.style.width = `${gridWidth / squareNum}`;
-    div.style.height = `${gridWidth / squareNum}`;
+    div.style.width = `${gridWidth / squareAmount}px`;
+    div.style.height = `${gridWidth / squareAmount}px`;
     gridContainer.appendChild(div);
 
     totalSquares++;
   }
 }
 
-buildGrid();
+//Initial building of grid when page loads
+
+buildGrid(squareNum);
+
+//================================================================
+
+//Event listener for when change grid button is pushed.  Sends prompt to ask user how many squares they want grid to be, and rebuilds grid based on this
+
+changeGridButton.addEventListener("click", function () {
+  gridContainer.replaceChildren();
+
+})
